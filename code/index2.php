4c0c1c75715de2ba03a6ea628481db3984712542
”Dorian <dorian.mahnke@gmail.com>
2016-09-12 13:33:43 +0200

    deleted old index, now redirecting to web

--- code/index2.php
+++
@@ -1,9 +0,0 @@
-<?php
-	/*if(file_exists('installation')) {
-		//header('Location: installation/index.php');
-	}
-	else {
-		//header('Location: web/index.php');
-	}*/
-	header('Location: web/index.php');
-?>
